39f770b39b56d8f6f9d258494197abfb530762a3
Joshua Parsels <jparsels@expedia.com>
2018-05-30 17:28:56 -0700

    first

--- fileForConflicts.js
+++ fileForConflicts.js
@@ -4,13 +4,13 @@ function printSethsMessage(inputString) {
 	console.log(inputString);
 }
 
-printSethsMessage("This is not Seth's Message, Don't fuck with it!");
+printSethsMessage("This is Seth's Message, Don't fuck with it!");
 
 function printLeosMessage(inputString) {
 	console.log(inputString);
 }
 
-printLeosMessage("This is not Josh's Message, Don't fuck with it!");
+printLeosMessage("This is Josh's Message, Don't fuck with it!");
 
 function helloThere() {
 	console.log("go away");
